32e8fc4b2594bec0b5cf9015751390edbfaf3b16
Harry <matheusthebr@gmail.com>
2021-10-23 14:57:08 -0300

    start app with pre and pos plans.

--- .elixir_ls/build/test/lib/telefonia/ebin/telefonia.app
+++
@@ -1,6 +0,0 @@
-{application,telefonia,
-             [{applications,[kernel,stdlib,elixir,logger]},
-              {description,"telefonia"},
-              {modules,['Elixir.Assinante','Elixir.Telefonia']},
-              {registered,[]},
-              {vsn,"0.1.0"}]}.
--- lib/assinante.ex
+++ lib/assinante.ex
@@ -1,18 +1,20 @@
 defmodule Assinante do
-  defstruct nome: nil, numero: nil, cpf: nil
+  defstruct nome: nil, numero: nil, cpf: nil, plano: nil
 
-  def cadastrar(nome, numero, cpf) do
-    (read() ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf}])
+  @assinantes %{:prepago => "pre.txt", :pospago => "pos.txt"}
+
+  def cadastrar(nome, numero, cpf, plano \\ :prepago) do
+    (read(plano) ++ [%__MODULE__{nome: nome, numero: numero, cpf: cpf, plano: plano}])
     |> :erlang.term_to_binary()
-    |> write()
+    |> write(plano)
   end
 
-  defp write(lista_de_assinantes) do
-    File.write!("assinantes.txt", lista_de_assinantes)
+  defp write(lista_de_assinantes, plano) do
+    File.write!(@assinantes[plano], lista_de_assinantes)
   end
 
-  def read() do
-    {:ok, assinantes} = File.read("assinantes.txt")
+  def read(plano) do
+    {:ok, assinantes} = File.read(@assinantes[plano])
 
     assinantes
     |> :erlang.binary_to_term()
