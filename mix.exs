4fdf411d8d615a5fe3b279f25478f0da0874f4c1
Harry <matheusthebr@gmail.com>
2021-11-13 18:16:55 -0300

    Added coveralls.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,14 @@ defmodule Telefonia.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
     ]
   end
 
@@ -24,7 +31,8 @@ defmodule Telefonia.MixProject do
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:earmark, "~> 1.3", only: :dev},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
